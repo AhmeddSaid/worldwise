b238f41cdda78640e99f96efe3a2dccbec081aea
Ahmed Said <ahmedsaidadnan@outlook.com>
2023-10-31 02:40:40 +0200

    minor update

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -19,7 +19,10 @@ export default function Product() {
             voluptatem iste.
           </p>
         </div>
-        <img src="img-2.jpg" alt="overview of a large city with skyscrapers" />
+        <img
+          src="/worldwise/assets/images/img-2.jpg"
+          alt="overview of a large city with skyscrapers"
+        />
       </section>
     </main>
   );
